4bde16e64c38d42b7e7c2d569ad066582af4cc68
itspritam008 <pritampaul2k17@gmail.com>
2024-02-05 14:10:52 +0530

    Code

---
+++ Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n==0)
+        return false;
+        int k =(int)(Math.log(n)/Math.log(2));
+        if (Math.pow(2,k)==n)
+        return true;
+        else
+        return false;
+    }
+}
